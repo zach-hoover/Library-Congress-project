5a79c76bff1f4452769e3cc87b18a4654e2f195b
zach <zachhoover126@yahoo.com>
2023-07-10 21:16:00 -0400

    added stuff

---
+++ script.js
@@ -0,0 +1,6 @@
+var submitEl= document.querySelector('submit')
+var searchEl= document.querySelector('search')
+var resultsEl= document.querySelector('results')
+var dropdownEl= document.querySelector('options')
+var requesturl= 
+fetch()
